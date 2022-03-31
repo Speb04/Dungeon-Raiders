c0b3cb6e054b4be9913314aba1b58a1515cb4936
xIEUx <joe.william.spedding@outlook.com>
2022-03-31 18:43:40 +0100

    Setting game up at home, updating journal

--- Assets/Scripts/Bats.cs
+++ Assets/Scripts/Bats.cs
@@ -4,18 +4,19 @@ public class Bats : MonoBehaviour
 {
     public BatScript[] prefabs;
     
-    public int rows = 5;
+    public int rows = 5; //number of bats going up
 
-    public int columns = 11;
+    public int columns = 11; //number of bats across
+    
+    public float speed = 10.0f; //speed
 
-    public float speed = 10.0f;
-
-    private Vector3 _direction = Vector2.right;
+    private Vector3 _direction = Vector2.right; //moves bats right
 
     private void Awake()
     {
         for (int row = 0; row < this.rows; row++)
         {
+            //creates a grids for the bats to be placed into
             float width = 8.0f * (this.columns - 1);
             float height = 8.0f * (this.rows - 1);
             Vector2 centering = new Vector2(-width / 2, -height /2);
@@ -23,6 +24,7 @@ public class Bats : MonoBehaviour
 
             for (int col = 0; col < this.columns; col++)
             {
+                //makes an array for the bat prefabs to be placed into a grid, can edit the number of bats spawned in grid through unity inspector
                 BatScript bat = Instantiate(this.prefabs[row], this.transform);
                 Vector3 position = rowPosition;
                 position.x += col * 8.0f;
@@ -31,6 +33,8 @@ public class Bats : MonoBehaviour
         }
 
     }
+
+    //used to make the Bats move, stops frame rate from having an affect on them
 
     private void Update()
     {
