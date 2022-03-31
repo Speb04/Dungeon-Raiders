a0f8d6fc031c6d84c095d16c03b98d29270422e7
xIEUx <joe.william.spedding@outlook.com>
2022-03-31 19:58:05 +0100

    New sprites and scripts

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Projectile laserPrefab;
+
+    public float speed = 5.0f;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) {
+            this.transform.position += Vector3.left * this.speed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.D)) {
+            this.transform.position += Vector3.right * this.speed * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Instantiate(this.laserPrefab, this.transform.position, Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public Vector3 direction;
+
+    public float speed;
+
+    private void Update()
+    {
+        this.transform.position = +this.direction * this.speed * Time.deltaTime;
+    }
+
+}
