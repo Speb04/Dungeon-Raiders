781d70287fe33b9abc311b1bd48585a645f1a100
xIEUx <joe.william.spedding@outlook.com>
2022-04-01 20:29:45 +0100

    New scripts and sprites, speed mechanics and bug fixed

--- Assets/Scripts/BatScript.cs
+++ Assets/Scripts/BatScript.cs
@@ -3,5 +3,14 @@ using UnityEngine;
 public class BatScript : MonoBehaviour
 {
 
+    public System.Action killed;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Fireball")) {
+            this.killed.Invoke();
+            this.gameObject.SetActive(false);
+        }
+    }
 
 }
--- Assets/Scripts/Bats.cs
+++ Assets/Scripts/Bats.cs
@@ -8,9 +8,15 @@ public class Bats : MonoBehaviour
 
     public int columns = 11; //number of bats across
     
-    public float speed = 10.0f; //speed
+    public AnimationCurve speed; //speed
 
     private Vector3 _direction = Vector2.right; //moves bats right
+
+    public int amountKilled { get; private set; } //counts amount killed
+
+    public int totalBats => this.rows * this.columns; //calculates percentage of bats
+
+    public float percentKilled => (float)this.amountKilled / (float)this.totalBats; //calculates total bats killed
 
     private void Awake()
     {
@@ -26,6 +32,7 @@ public class Bats : MonoBehaviour
             {
                 //makes an array for the bat prefabs to be placed into a grid, can edit the number of bats spawned in grid through unity inspector
                 BatScript bat = Instantiate(this.prefabs[row], this.transform);
+                bat.killed += BatKilled;
                 Vector3 position = rowPosition;
                 position.x += col * 8.0f;
                 bat.transform.localPosition = position; 
@@ -38,7 +45,7 @@ public class Bats : MonoBehaviour
 
     private void Update()
     {
-        this.transform.position += _direction * this.speed * Time.deltaTime;
+        this.transform.position += _direction * this.speed.Evaluate(this.percentKilled) * Time.deltaTime;
 
         //when bats touch edge of main camera...
         Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
@@ -68,6 +75,11 @@ public class Bats : MonoBehaviour
         Vector3 position = this.transform.position;
         position.y -= 1.0f;
         this.transform.position = position;
+    }
+
+    private void BatKilled()
+    {
+        this.amountKilled++;
     }
 
 }
