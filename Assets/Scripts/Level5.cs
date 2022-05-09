72dff9cabd71a352f58e25b8c180448ecd575b99
Speb <joe.william.spedding@outlook.com>
2022-05-09 15:02:21 +0100

    wtf

--- Assets/Scripts/Level5.cs
+++ Assets/Scripts/Level5.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level1 : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class Level5 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void LoadLevel5  ()
+    {
+        SceneManager.LoadScene("Level 5");
     }
 }
+
